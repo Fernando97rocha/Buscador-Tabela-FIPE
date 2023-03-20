851073fa772167dba4c4bb0ac0bd6ae30e7693cb
Fernando Rodrigues <nandocf20@gmail.com>
2023-03-20 02:16:43 -0300

    ajustes visuais da aplicação

--- Buscador-Tabela-FIPE/appRequest.js
+++ Buscador-Tabela-FIPE/appRequest.js
@@ -4,107 +4,108 @@ const spanModelo = document.querySelector('.span-modelo')
 const spanAnoModelo = document.querySelector('.span-ano-modelo')
 
 
-btnConsultar.addEventListener('click', (marca, valorModelo, valorAnos) => {
-  const urlModeloAno = (codigoMarca) => 
+btnConsultar.addEventListener('click', (event, marca, valorModelo, valorAnos) => {
+
+  if (listaModelos.value !== 'default-modelo' && listaAnos.value !== 'default-ano') {
+    divResultado.setAttribute('class', 'mostra')
+    ul.setAttribute('class', 'mostra')
+  }
+  
+  const urlModeloAno = (codigoMarca) =>
     `https://parallelum.com.br/fipe/api/v1/carros/marcas/${codigoMarca}/modelos`
-  
-  const urlValores = (codigoMarca, codigoModelo, codigoAno) => 
+
+  const urlValores = (codigoMarca, codigoModelo, codigoAno) =>
     `https://parallelum.com.br/fipe/api/v1/carros/marcas/${codigoMarca}/modelos/${codigoModelo}/anos/${codigoAno}`
-  
+
   const FuncMarca = async (url) => {
-      try { 
+    marca = listaMarcas.value
+    valorModelo = listaModelos.value
+    valorAnos = listaAnos.value
+    try {
 
-        marca = await listaMarcas.value
-        valorModelo = await listaModelos.value
-        valorAnos = await listaAnos.value
-
-        const response = await fetch(urlMarca(url))
-        if(!response.ok) {
-          throw new Error('Não foi possível obter os dados')
-        }
-
-        const carros = await response.json()
-        console.log(marca)
-        carros.filter(carro => {
-          if (marca === carro.nome) {
-            funcModelos = async () => {
-              console.log(await carro)
-              const codigoMarca = carro.codigo
-              
-              try {
-                console.log(codigoMarca)
-                const response = await fetch(urlModeloAno(codigoMarca))
-                if (!response.ok) {
-                  throw new Error('Não foi possível obter os dados da API')
-                }
-                
-                const anosModelos = await response.json()
-                console.log(anosModelos)
-                
-                anosModelos.modelos.filter((modelo) => {
-                  if (valorModelo === modelo.nome) {
-                    console.log(modelo.nome)
-                    const codigoModelo = modelo.codigo
-
-                    const funcAnos = () => {
-                      anosModelos.anos.filter(ano => {
-                        if (valorAnos === ano.nome) {
-                          console.log(ano.nome)
-                          const codigoAno = ano.codigo
-
-                          const obtemValores = async () => {
-                            console.log(codigoMarca, codigoModelo, codigoAno)
-                            
-                            try {
-                              const response = await fetch(urlValores(codigoMarca, 
-                                codigoModelo, 
-                                codigoAno))
-                              
-                              if (!response.ok) {
-                                throw new Error('Não foi possível obter os valores')
-                              }
-
-                              const objValores = await response.json()
-                              console.log(objValores)
-                              
-                              spanMarca.innerText = `${objValores.Marca}`
-                              spanModelo.innerText = `Modelo: ${objValores.Modelo}`
-                              if (objValores.AnoModelo === 32000) {
-                                spanAnoModelo.innerText = `0 km`
-                              } else {
-                                spanAnoModelo.innerText = `Ano: ${objValores.AnoModelo}`
-                              }
-                              spanValor.innerText = `${objValores.Valor}`
-
-                            } catch (error) {
-                              alert(error)
-                            }
-                          }
-                          obtemValores()
-
-                        }
-                      })
-                    }
-                    funcAnos()
-
-                  }
-                })
-
-              } catch (error) {
-                alert(error)
-              }
-            }
-            funcModelos()
-            
-          }
-        })
-
-      } catch (error) {
-        alert(error)
+      const response = await fetch(urlMarca(url))
+      if (!response.ok) {
+        throw new Error('Não foi possível obter os dados')
       }
+
+      const carros = await response.json()
+      carros.filter(carro => {
+        if (marca === carro.nome) {
+          funcModelos = async () => {
+            const codigoMarca = carro.codigo
+
+            try {
+              const response = await fetch(urlModeloAno(codigoMarca))
+              if (!response.ok) {
+                throw new Error('Não foi possível obter os dados da API')
+              }
+
+              const anosModelos = await response.json()
+
+              anosModelos.modelos.filter((modelo) => {
+                if (valorModelo === modelo.nome) {
+                  const codigoModelo = modelo.codigo
+
+                  const funcAnos = () => {
+                    anosModelos.anos.filter(ano => {
+                      if (valorAnos === ano.nome) {
+                        const codigoAno = ano.codigo
+
+                        const obtemValores = async () => {
+
+                          try {
+                            const response = await fetch(urlValores(codigoMarca,
+                              codigoModelo,
+                              codigoAno))
+
+                            if (!response.ok) {
+                              throw new Error('Não foi possível obter os valores')
+                            }
+
+                            const objValores = await response.json()
+
+                            spanMarca.innerText = `${objValores.Marca}`
+                            spanModelo.innerText = `Modelo: ${objValores.Modelo}`
+                            if (objValores.AnoModelo === 32000) {
+                              spanAnoModelo.innerText = `0 km`
+                            } else {
+                              spanAnoModelo.innerText = `Ano: ${objValores.AnoModelo}`
+                            }
+                            spanValor.innerText = `${objValores.Valor}`
+
+                            if (event.target === btnConsultar) {
+                              divResultado.setAttribute('class', 'mostra')
+                              ul.setAttribute('class', 'mostra')
+                            }
+                          } catch (error) {
+                            alert(error)
+                          }
+                        }
+                        obtemValores()
+
+                      }
+                    })
+                  }
+                  funcAnos()
+
+                }
+              })
+
+            } catch (error) {
+              alert(error)
+            }
+          }
+          funcModelos()
+
+        }
+      })
+
+    } catch (error) {
+      alert(error)
+    }
   }
 
   FuncMarca()
 
-  
+
 })
--- Buscador-Tabela-FIPE/formRequest.js
+++ Buscador-Tabela-FIPE/formRequest.js
@@ -5,62 +5,72 @@ const listaAnos = document.querySelector('#anos')
 const btnBuscar = document.querySelector('#buscar')
 const btnConsultar = document.querySelector('#consultar')
 const divResultado = document.querySelector('#resultadoBusca')
+const ul = document.querySelector('ul')
 
 
-const urlMarca = url =>  url = 'https://parallelum.com.br/fipe/api/v1/carros/marcas'
+const urlMarca = url => url = 'https://parallelum.com.br/fipe/api/v1/carros/marcas'
 
-const urlModelo = codigoMarca => 
+const urlModelo = codigoMarca =>
   `https://parallelum.com.br/fipe/api/v1/carros/marcas/${codigoMarca}/modelos`
 
-const urlModeloAno = (codigoMarca, codigoModelo) => 
+const urlModeloAno = (codigoMarca, codigoModelo) =>
   `https://parallelum.com.br/fipe/api/v1/carros/marcas/${codigoMarca}/modelos/${codigoModelo}/anos`
-  
+
 const urlValorVeiculo = codigoAno => `https://parallelum.com.br/fipe/api/v1/carros/marcas/${urlModelo()}/modelos/${urlAno()}/anos/${codigoAno}`
 
 const obtemMarca = async (url) => {
-  
+
   const stringInicialMarcas = '- Selecione uma marca -'
-  try{
+  try {
     const response = await fetch(urlMarca(url))
-    console.log(urlMarca(url))
     if (!response.ok) {
       throw new Error('Erro Request Marca')
     }
     const carros = await response.json()
-    const mostraMarcasNoInput = async() => {
-      
+    const mostraMarcasNoInput = async () => {
+
       listaMarcas.innerHTML += `<option value="default-marca">${stringInicialMarcas}</option>`
-      carros.forEach(async carro => { 
+      carros.forEach(async carro => {
         listaMarcas.innerHTML += `<option value="${carro.nome}">${carro.nome}</option>`
       })
     }
     mostraMarcasNoInput()
-
-    if(listaMarcas.value) {
-      btnBuscar.addEventListener('click', () => {
-        console.log(listaMarcas.value)
-
-        listaModelos.setAttribute('class', 'mostra-modelos')
-        listaAnos.setAttribute('class', 'mostra-modelos')
+ 
+    if (listaMarcas.value) {
+      btnBuscar.addEventListener('click', event => {
+        if (listaMarcas.value === 'default-marca') {
+        } else {
+          listaModelos.setAttribute('class', 'mostra')
+          listaAnos.setAttribute('class', 'mostra')
+          btnConsultar.setAttribute('class', 'mostra')
+        }
+        
+        if (event.target === btnBuscar) {
+          
+          divResultado.setAttribute('class', 'ocultado')
+          ul.setAttribute('class', 'ocultado')
+          
+          console.log(event.target)
+        }
 
         if (listaModelos.value && listaAnos.value) {
           listaModelos.innerHTML = ''
           listaAnos.innerHTML = ''
           //if que reseta os campos caso o usuário queira trocar a marca do carro
         }
-        
+
         carros.filter(async carro => {
           //filter para capturar somente o valor escolhido pelo usuário
-          if(listaMarcas.value === carro.nome) {
-            
+          if (listaMarcas.value === carro.nome) {
+
             const codigoMarca = carro.codigo
-              
+
             const obtemModelo = async () => {
-              
-              try { 
+
+              try {
                 const response = await fetch(urlModelo(codigoMarca))
 
-                if(!response.ok) {
+                if (!response.ok) {
                   throw new Error('Não foi possível obter os dados')
                 }
 
@@ -73,7 +83,7 @@ const obtemMarca = async (url) => {
                   })
                 }
                 mostraModelos()
-  
+
                 const mostraAnos = () => {
                   //função que disponibiliza as opções dos modelos de cada marca
                   listaAnos.innerHTML += `<option value="default-ano">- Selecione um ano -</option>`
@@ -81,26 +91,23 @@ const obtemMarca = async (url) => {
                     listaAnos.innerHTML += `<option value="${ano.nome}">${ano.nome}</option>`
                   })
                 }
-  
                 mostraAnos()
-  
+
               } catch (error) {
                 alert(error)
               }
-  
             }
             obtemModelo()
-              
+
           }
         })
-    })
-        
-  }
+      })
+    }
 
-  }catch (error) {
+  } catch (error) {
     alert(error)
   }
-    
+
 }
 obtemMarca()
 
